c2179ccb0bd4d9f38a17f843f5365db55d5740e1
AtulKhajuria <atulkhajuria234@gmail.com>
2026-07-17 00:39:50 +0530

    Fixed backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "atul-terraform-state-2026"
-    key = "terraform-projects/terraform-aws-ha-apache"
+    key = "terraform-projects/terraform-aws-ha-apache/terraform.tfstate"
     region = "ap-south-1"
     use_lockfile = true
   }
