2217de752665169e9166d699682f9776a216ce27
AtulKhajuria <atulkhajuria234@gmail.com>
2026-07-20 02:07:08 +0530

    Added network module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -25,3 +25,19 @@ resource "aws_subnet" "public" {
     Name = "${var.project_name}-${var.environment}-${each.key}"
   }
 }
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    Name = "${var.project_name}-${var.environment}-public_rt"
+  }
+}
+resource "aws_route" "internet_access" {
+  route_table_id = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.this.id
+}
+resource "aws_route_table_association" "public_subent" {
+  for_each = aws_subnet.public
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id = each.value[aws_subnet.public.id]
+}
